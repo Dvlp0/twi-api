8498be510c42cf8b29e73a4e807411584d55c117
Eakkaphop pengtham <34792704+Dvlp0@users.noreply.github.com>
2019-03-01 03:45:12 +0700

    Add files via upload

--- tweet-with-media.php
+++ tweet-with-media.php
@@ -13,7 +13,8 @@ if (!isset($_SESSION['access_token'])) {
 	$_SESSION['oauth_token'] = $request_token['oauth_token'];
 	$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
 	$url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
-	echo $url;
+	//echo $url;
+	echo "ท่านยังไม่ได้ login<br><a href='$url'>กรูณา login</a>";
 } else {
 	$access_token = $_SESSION['access_token'];
 	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
